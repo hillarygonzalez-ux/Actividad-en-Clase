250fc1fdd709e804040904a04163198fad082c1d
unknown <hillary.gonzalez@correounivalle.edu.co>
2026-02-26 12:14:11 -0500

    agregar division.html

--- app.py
+++ app.py
@@ -18,6 +18,26 @@ def sumar():
 
     return render_template('suma.html')
 
+@app.route('/division', methods=['GET', 'POST'])
+def dividir():
+    resultado = None
+    error = None
+
+    if request.method == 'POST':
+        try:
+            num1 = float(request.form.get('num1'))
+            num2 = float(request.form.get('num2'))
+
+            if num2 == 0:
+                error = "No se puede dividir entre 0"
+            else:
+                resultado = num1 / num2
+
+        except (TypeError, ValueError):
+            error = "Por favor ingresa números válidos"
+
+    return render_template('division.html', resultado=resultado, error=error)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
